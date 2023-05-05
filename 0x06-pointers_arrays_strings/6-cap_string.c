59d16a860a74670dd291ff91d588c6763ca3d1e0
Faith Njambi <njambifaith30@gmail.com>
2023-05-05 17:21:01 +0300

    :bug: fix requirement fail

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,13 +8,12 @@
 char *cap_string(char *s)
 {
 	int count = 0, i;
-
 	/* ASCII for ,, ;, ., !, ?, ", (, ), {, and } */
 	int separators[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
 	if (*(s + count) >= 97 && *(s + count) <= 122)
 		*(s + count) = *(s + count) - 32;
-		count++;
+	count++;
 	while (*(s + count) != '\0')
 	{
 		for (i = 0; i < 13; i++)
@@ -23,10 +22,11 @@ char *cap_string(char *s)
 				{
 					if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
 						*(s + (count + 1)) = *(s + (count + 1)) - 32;
-						break;
+					break;
 				}
 		}
 		count++;
 	}
+
 	return (s);
 }
