a1a1130b70df498785e30e18d83e64b761f8c293
Faith Njambi <njambifaith30@gmail.com>
2023-05-08 21:50:53 +0300

    :bug: fix betty

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -5,13 +5,14 @@
 * @dest: memory area to be copied to
 * @src: memory area to be copied from
 * @n: number of bytes
+* Return: pointer to dest
 */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 unsigned int i;
 
-    for (i = 0; i < n; i++)
-        *(dest + i) =  *(src + i);
+	for (i = 0; i < n; i++)
+	*(dest + i) =  *(src + i);
 
-    return (dest);
+	return (dest);
 }
