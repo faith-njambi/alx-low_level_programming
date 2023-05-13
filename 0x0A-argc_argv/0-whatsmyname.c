b7f40050dd968d8240a225f89474ba1290ee8997
Faith Njambi <njambifaith30@gmail.com>
2023-05-13 05:22:22 +0300

    a program that prints the number of arguments passed into it

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,8 +6,10 @@
 * @argv: array of strings
 * Return: 0
 */
-int main (int __attribute__((unused)) argc, char *argv[])
+int main (int argc, char *argv[])
 {
+	(void) argc;
 	printf("%s\n", argv[0]);
+
 	return (0);
 }
