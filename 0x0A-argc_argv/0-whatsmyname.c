be46e185b47bf4eccf5da6d8230459d7d51c8e67
Faith Njambi <njambifaith30@gmail.com>
2023-05-13 22:13:06 +0300

    correction

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,9 +4,9 @@
 * main - prints pragram name
 * @argc: number of arguments
 * @argv: array of arguments
-* Return: 0
+* Return: success
 */
-int main (int argc __attribute__((unused)), char *argv[])
+int main (int __attribute__((__unused__)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 
