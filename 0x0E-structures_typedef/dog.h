276433ced9a764801dbf5f5b3ad1c98372135f21
Faith Njambi <njambifaith30@gmail.com>
2023-05-22 22:41:52 +0300

    to add structure documentation

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog 
+/**
+* struct dog - contains elements of the dog
+* @name: name of the dog
+* @age: age of the dog
+* @owner: owner of the dog
+*
+* Description: struct for a dog
+*/
+struct dog
 {
 	char *name;
 	float age;
