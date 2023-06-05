5905d16005ebc06f6a271484bca2c61f1a05571c
Faith Njambi <njambifaith30@gmail.com>
2023-06-05 16:42:18 -0700

    :bug: Fix requirement fail

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,16 +1,15 @@
 #include "lists.h"
 
 /**
- * print_listint -  prints all the elements of a list_t list
- * @h: head of list node
+ * print_listint -  prints all the elements of a listint_t list
+ * @h: linked list
  * Return: number of nodes
  */
-
 size_t print_listint(const listint_t *h)
 {
 	size_t nodes = 0;
 
-	while (h != NULL)
+	while (h)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
