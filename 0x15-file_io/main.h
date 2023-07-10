e58f330966d734b105e4c5e633ad4e5038166050
Faith Njambi <njambifaith30@gmail.com>
2023-07-10 23:45:54 +0300

    :white_check_mark: to include guard the header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,4 +1,9 @@
+#ifndef READ_TEXTFILE_H
+#define READ_TEXTFILE_H
+
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+
+#endif
