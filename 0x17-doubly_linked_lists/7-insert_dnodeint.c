1597f286794d3f42d08d92f164cf9659afe96db1
Faith Njambi <njambifaith30@gmail.com>
2023-08-17 20:25:25 +0300

    :wrench: To fix betty fail

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -24,7 +24,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		newNode->prev = NULL;
 		if (*h != NULL)
 			(*h)->prev = newNode;
-		*h =newNode;
+		*h = newNode;
 		return (newNode);
 	}
 
